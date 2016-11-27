e59dd7b8173cfb5bee44abbaed3870a17197fa94
Julia Connelly <juliakconnelly@gmail.com>
2016-11-27 17:24:15 +0100

    can build houses

---
+++ HousingCrisis/Assets/Scripts/PlotController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class PlotController : MonoBehaviour {
+
+    public GameObject house;
+
+    private bool open;
+    private Vector2 mousePos;
+    private Vector3 screenPos;
+    private Rect menuRect;
+
+    void Awake() {
+        open = false;
+        screenPos = Camera.main.WorldToScreenPoint(transform.position);
+    }
+
+    void OnMouseDown() {
+        open = true;
+        mousePos = Input.mousePosition;
+    }
+
+    void OnGUI() {
+        if(open) {
+            GUI.Box(new Rect(mousePos, new Vector2(100, 100)), "");
+            if(GUI.Button(new Rect(mousePos, new Vector2(100, 50)), "Build House")) {
+                Instantiate(house, transform.position, Quaternion.identity);
+                Destroy(gameObject);
+            }
+            if(GUI.Button(new Rect(mousePos[0], mousePos[1] + 50, 100, 50), "Close")) {
+                open = false;
+            }
+        }
+    }
+}
