654f4f78b078a8efc6b355175cd64b865b41da84
Julia Connelly <juliakconnelly@gmail.com>
2016-12-03 20:44:22 +0100

    added an info pop up - drag the prefab into the scene you want it in and enter the text  you want under words (each index in the array changes the text in the bubble)

---
+++ HousingCrisis/Assets/Scripts/Specials/SpeechBubble.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SpeechBubble : MonoBehaviour {
+
+    public Text speechText;
+    public Button okButton;
+    public string[] words;
+
+    private int wordIndex = 0;
+
+    void Start() {
+        speechText.text = words[wordIndex];
+        wordIndex++;
+        okButton.onClick.AddListener(() => {
+            if(!NextText()) Destroy(gameObject);
+        });
+    }
+
+    private bool NextText() {
+        if(wordIndex < words.Length) {
+            speechText.text = words[wordIndex];
+            wordIndex++;
+            return true;
+        }
+        return false;
+    }
+}
