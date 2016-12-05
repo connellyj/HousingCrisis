059cd31ce39842cc6c71f7cbc70d79b2b8b8d8a7
Julia Connelly <juliakconnelly@gmail.com>
2016-12-05 01:37:23 +0100

    working on tutorials, stopping to look at cops

---
+++ HousingCrisis/Assets/Scripts/Managers/TutorialManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class TutorialManager : MonoBehaviour {
+
+    public GameObject speechBubblePrefab;
+
+    private SpeechBubble speechBubble;
+    //private int moneyToPopAt = 
+
+    void Start() {
+        speechBubble = (Instantiate(speechBubblePrefab)).GetComponent<SpeechBubble>();
+        speechBubble.okButton.onClick.AddListener(() => Destroy(speechBubble.gameObject));
+        speechBubble.words = new string[1] {"Hello! My name is Hubert Houserton! *insert euphamism* Click on a plot " +
+            "of land to build a house, then click the people as they walk by!"};
+        speechBubble.NextText();
+    }
+}
--- HousingCrisis/Assets/Scripts/Specials/SpeechBubble.cs
+++ HousingCrisis/Assets/Scripts/Specials/SpeechBubble.cs
@@ -6,19 +6,22 @@ public class SpeechBubble : MonoBehaviour {
     public Text speechText;
     public Button okButton;
     public string[] words;
+    public bool tutorial;
 
     private int wordIndex = 0;
 
     void Start() {
-        speechText.text = words[wordIndex];
-        wordIndex++;
-        okButton.onClick.AddListener(() => {
-            if(!NextText()) Destroy(gameObject);
-        });
+        if(!tutorial) {
+            speechText.text = words[wordIndex];
+            wordIndex++;
+            okButton.onClick.AddListener(() => {
+                if(!NextText()) Destroy(gameObject);
+            });
+        }
     }
 
     // Switches to the next section of text or destroys the bubble when there's no more text
-    private bool NextText() {
+    public bool NextText() {
         if(wordIndex < words.Length) {
             speechText.text = words[wordIndex];
             wordIndex++;
